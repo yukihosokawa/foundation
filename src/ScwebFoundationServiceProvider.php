de4eeace1802bc508ad7e1d5e98ed124bed7b21e
yukihosokawa <yuki.hosokawa@entertainovation.co.jp>
2024-03-14 00:46:35 +0900

    first commit

---
+++ src/ScwebFoundationServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Scweb\Foundation;
+
+use Illuminate\Support\ServiceProvider;
+
+class ScwebFoundationServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
